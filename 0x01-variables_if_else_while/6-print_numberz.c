9494e6ee2afa03be8cba1ddfcfbf66d92cf18026
oga pita <okechukwupeters32@gmail.comgit config --global user.email okechukwupeters32@gmail.com>
2022-10-16 08:38:20 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
