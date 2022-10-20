b2fa5e84d74dce33caf683db0a60667b08140831
oga pita <okechukwupeters32@gmail.comgit config --global user.email okechukwupeters32@gmail.com>
2022-10-19 19:24:04 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * _isalpha - chects for character alphabet
- * @c: the character to be checked
- * Return: 1 if character is lowercase or uppercase, otherwise 0
+ * _isalpha - check if char is lowercase or uppercase
+ * @c: the char to be checked
+ * Return: 1 if char is lowercase or uppercase, otherwise 0
  */
 
 int _isalpha(int c)
 
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
-		return (0)
+		return (0);
 
 
 }
