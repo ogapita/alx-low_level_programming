3da826a407cc672e17ddb51195d8868e3919fb90
oga pita <okechukwupeters32@gmail.comgit config --global user.email okechukwupeters32@gmail.com>
2022-10-20 06:24:25 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - teste function that prints if integer is psoitive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	printf("%d is positive\n", i);
+
+	return (0);
+}
