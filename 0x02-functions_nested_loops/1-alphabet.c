8e4cef95d862222ef842674dfea5b3529ce0f361
oga pita <okechukwupeters32@gmail.comgit config --global user.email okechukwupeters32@gmail.com>
2022-10-19 18:12:02 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  * print_alphabet - lowercase alphabets
  */
 
-void print_alphabet(void);
-
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 
 
