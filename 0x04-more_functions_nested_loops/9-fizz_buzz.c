ecd910db87b964dced9882bca42e3df5d2fd5080
oga pita <okechukwupeters32@gmail.comgit config --global user.email okechukwupeters32@gmail.com>
2022-10-21 14:48:52 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,6 +18,10 @@ int main(void)
 		{
 			printf(" Fizz");
 		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
