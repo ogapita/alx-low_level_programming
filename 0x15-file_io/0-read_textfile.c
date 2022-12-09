ac3749438d70abc8daaf08a19c7baf307b9bf95b
oga pita <okechukwupeters32@gmail.comgit config --global user.email okechukwupeters32@gmail.com>
2022-12-09 02:31:50 -0800

    corrected task zero

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,6 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
